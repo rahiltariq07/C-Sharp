88de7ebb4baebf19c46112efaa185a8c336a6f54
rahiltariq07 <rahiltariq07@gmail.com>
2024-06-07 12:12:25 +0530

    assignment-1

---
+++ Assignment-1/Assignment-1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the first number:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Before swap:");
+Console.WriteLine($"a = {a}, b = {b}");
+
+// Swap logic
+int temp = a;
+a = b;
+b = temp;
+
+Console.WriteLine("After swap:");
+Console.WriteLine($"a = {a}, b = {b}");
